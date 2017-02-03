00ebce1e515b9c62ef3faa6140e61d178fcaaa0d
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-02-03 09:37:37 +0000

    add new hello.py

--- HelloFlask/hello.py
+++ HelloFlask/hello.py
@@ -4,9 +4,22 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello.html")
 def hello():
     return "Hello World!"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Port %d' %post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
 
 if __name__ == "__main__":
     app.run()
